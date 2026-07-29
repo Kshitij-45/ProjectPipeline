afe0ca768da34b2cc2ae2f3d6f1f2c6bf4b6c7fb
Kshitij-45 <soham.zure@gmail.com>
2026-07-29 09:38:03 +0000

    Comment out Cricket class implementation
    Commented out the entire Cricket class code.

--- src/main/java/com/example/model/Cricket.java
+++ src/main/java/com/example/model/Cricket.java
@@ -1,41 +1,41 @@
-package com.example.model;
+// package com.example.model;
 
-public class Cricket {
-      private int id;
-      private String name;
-      private String country;
-      private int runs;
-      public Cricket() {
-      }
-      public Cricket(int id, String name, String country, int runs) {
-        this.id = id;
-        this.name = name;
-        this.country = country;
-        this.runs = runs;
-      }
-      public int getId() {
-          return id;
-      }
-      public void setId(int id) {
-          this.id = id;
-      }
-      public String getName() {
-          return name;
-      }
-      public void setName(String name) {
-          this.name = name;
-      }
-      public String getCountry() {
-          return country;
-      }
-      public void setCountry(String country) {
-          this.country = country;
-      }
-      public int getRuns() {
-          return runs;
-      }
-      public void setRuns(int runs) {
-          this.runs = runs;
-      }
+// public class Cricket {
+//       private int id;
+//       private String name;
+//       private String country;
+//       private int runs;
+//       public Cricket() {
+//       }
+//       public Cricket(int id, String name, String country, int runs) {
+//         this.id = id;
+//         this.name = name;
+//         this.country = country;
+//         this.runs = runs;
+//       }
+//       public int getId() {
+//           return id;
+//       }
+//       public void setId(int id) {
+      //     this.id = id;
+      // }
+      // public String getName() {
+      //     return name;
+      // }
+      // public void setName(String name) {
+      //     this.name = name;
+      // }
+      // public String getCountry() {
+      //     return country;
+      // }
+      // public void setCountry(String country) {
+      //     this.country = country;
+      // }
+      // public int getRuns() {
+      //     return runs;
+      // }
+      // public void setRuns(int runs) {
+      //     this.runs = runs;
+      // }
       
-}
+// }
