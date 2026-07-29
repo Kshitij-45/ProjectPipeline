3b24352bb3214e48456b3c729d9dc5a564f0a9d6
Kshitij-45 <soham.zure@gmail.com>
2026-07-29 09:38:55 +0000

    Comment out CricRepository class and methods
    Commented out the entire CricRepository class and its methods.

--- src/main/java/com/example/repository/CricRepository.java
+++ src/main/java/com/example/repository/CricRepository.java
@@ -1,59 +1,59 @@
-package com.example.repository;
-import java.util.List;
+// package com.example.repository;
+// import java.util.List;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+// import org.springframework.jdbc.core.BeanPropertyRowMapper;
+// import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.stereotype.Repository;
 
-import com.example.model.Cricket;
-@Repository
-public class CricRepository {
+// import com.example.model.Cricket;
+// @Repository
+// public class CricRepository {
       
       
-        private JdbcTemplate jdbcTemplate;
+//         private JdbcTemplate jdbcTemplate;
     
-            public CricRepository(JdbcTemplate jdbcTemplate) {
-                this.jdbcTemplate = jdbcTemplate;
-            }
+//             public CricRepository(JdbcTemplate jdbcTemplate) {
+//                 this.jdbcTemplate = jdbcTemplate;
+//             }
     
-            public int save(Cricket cricket) {
+//             public int save(Cricket cricket) {
     
-                String sql =
-                        "insert into cricket(name,country,runs) values(?,?,?)";
+            //     String sql =
+            //             "insert into cricket(name,country,runs) values(?,?,?)";
     
-                return jdbcTemplate.update(sql,
-                        cricket.getName(),
-                        cricket.getCountry(),
-                        cricket.getRuns());
-            }
+            //     return jdbcTemplate.update(sql,
+            //             cricket.getName(),
+            //             cricket.getCountry(),
+            //             cricket.getRuns());
+            // }
     
-            public List<Cricket> findAll() {
+            // public List<Cricket> findAll() {
     
-                String sql="select * from cricket";
+            //     String sql="select * from cricket";
     
-                return jdbcTemplate.query(sql,
-                        new BeanPropertyRowMapper<>(Cricket.class));
-            }
+            //     return jdbcTemplate.query(sql,
+            //             new BeanPropertyRowMapper<>(Cricket.class));
+            // }
     
-            public Cricket findById(int id){
+            // public Cricket findById(int id){
     
-                String sql="select * from cricket where id=?";
+            //     String sql="select * from cricket where id=?";
     
-                return jdbcTemplate.queryForObject(
-                        sql,
-                        new BeanPropertyRowMapper<>(Cricket.class),
-                        id);
-            }
+            //     return jdbcTemplate.queryForObject(
+            //             sql,
+            //             new BeanPropertyRowMapper<>(Cricket.class),
+            //             id);
+            // }
     
-            public int update(Cricket cricket){
+            // public int update(Cricket cricket){
     
-                String sql=
-                        "update cricket set name=?,country=?,runs=? where id=?";
+            //     String sql=
+            //             "update cricket set name=?,country=?,runs=? where id=?";
     
-                return jdbcTemplate.update(sql,
-                        cricket.getName(),
-                        cricket.getCountry(),
-                        cricket.getRuns (),
-                        cricket.getId());
-            }
-}
+            //     return jdbcTemplate.update(sql,
+            //             cricket.getName(),
+            //             cricket.getCountry(),
+            //             cricket.getRuns (),
+            //             cricket.getId());
+            // }
+//}
